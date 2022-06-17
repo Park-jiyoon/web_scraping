bc843ab4a8b8bc5bcb6ccdab16bf525dce8fc75f
Park-jiyoon <bbpgy1995@gmail.com>
2022-06-17 19:18:14 +0900

    getTitle 함수 생성

--- scrapetest.py
+++ scrapetest.py
@@ -3,18 +3,20 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
 
-try:
-    html = urlopen('http://pythonscraping.com/pages/page1.html')
-except HTTPError as e:
-    print(e)
-except URLError as e:
-    print('The server could not be found!')
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle('https://www.google.com/search?q=%EB%9D%BC%EC%BD%94%EC%8A%A4%ED%85%8C+%EA%B0%80%EB%94%94%EA%B1%B4&oq=%EB%9D%BC%EC%BD%94%EC%8A%A4%ED%85%8C+%EA%B0%80%EB%94%94%EA%B1%B4&aqs=chrome.0.0i512l7j69i60.2192j0j4&sourceid=chrome&ie=UTF-8')
+if title == None:
+    print('Title could not be found')
 else:
-    print('It Worked!')
-    print(bs.nonExistentTag)
-
-bs = BeautifulSoup(html.read(), 'html.parser')
-# bs = BeautifulSoup(html.read(), 'lxml')  # 지저분한 html 코드를 분석할 때
-
-print(bs.h1)
-
+    print(title)
